750db0b3d8b7286ee7d5fbe457e4b0cc9f54b199
MariaValerianoo <149009830+MariaValerianoo@users.noreply.github.com>
2024-02-19 08:13:39 -0500

    Create ejercicio2.cpp

---
+++ punteros/ejercicio2.cpp
@@ -0,0 +1,11 @@
+int	nombre,tipo,*a,edad,nombreDueño;	
+cout<<"Bienvenido a la veterinaria Maria Luz"<<endl;
+cout<<"Ingresa primero que tipo de animal es tu mascota: "<<endl;
+cin<<tipo;
+*a=tipo;
+cout<<"Ahora dinos el nombre: "<<endl;
+cin<<nombre;
+cout<<"Que edad tiene tu mascota? : "<<endl;
+cin<<edad;
+cout<<"Por ultimo cual es el nombre del dueño?: "<<endl;
+cin<<nombreDueño;
