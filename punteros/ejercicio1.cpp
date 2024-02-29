4aaa98aa77919256cf7c89910160958b4a24be91
MariaValerianoo <149009830+MariaValerianoo@users.noreply.github.com>
2024-02-29 04:55:07 -0500

    Update ejercicio1.cpp

--- punteros/ejercicio1.cpp
+++ punteros/ejercicio1.cpp
@@ -1,6 +1,6 @@
 int	x,	*p1,	*p2;	//declaramos los punteros y la variable x
 *p1	=	NULL;	//Le asignamos a los punteros un valor nulo
-*p2	=	NULL;	//Le asignamos a los punteros un valor nulo
+*p2	=	NULL;	
 x	=	15;	//a x le asignamos el valor de 15
 p1	=	&x;	//Al puntero p1 le vamos a asignar x que es 15
 p2	=	p1; //Y por ultimo vamos a asignarle a p2 el valor de p1 que seria 15 tambien
