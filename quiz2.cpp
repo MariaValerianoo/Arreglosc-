a788b6f0f729ca778c02a5e210d5b207212702f0
MariaValerianoo <149009830+MariaValerianoo@users.noreply.github.com>
2024-02-29 05:40:40 -0500

    update

--- quiz2.cpp
+++ quiz2.cpp
@@ -1,23 +1,46 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int cant1=0;
-    int suma=0;
-    cout<<"por favor ingrese la cantidad de minas que quiere ingresar en esta primera casilla: "<<endl;
-    cin>>cant1;
-    string numeros1[cant1];
-    for (int j = 0; j < cant1; j++)
-    {
-        cout<<"1)si y 2)no"<<endl;
-        cout<<"Ingresa si quieres mina: "<<endl;
-        cin>>numeros1[j];
-        if(j=1){
-            suma+=numeros1[j]
+
+int main() {
+    int cant1 = 0;
+    int suma = 0;
+
+    cout << "Por favor ingrese la cantidad de minas que quiere ingresar en esta primera casilla: " << endl;
+    cin >> cant1;
+    int numeros1[cant1];
+
+    for (int j = 0; j < cant1; j++) {
+        cout << "Ingrese 1 si hay mina y 0 si no hay mina: " << endl; 
+        cin >> numeros1[j];
+        if (numeros1[j] == 1) {
+            suma++;
         }
-     
+        while (numeros1[j]!=1 && numeros1[j]!=0)
+        {
+            cout<<"Numero incorrecto, vuelve a intentarlo\n";
+            cout << "Ingrese 1 si hay mina y 0 si no hay mina: " << endl; 
+            cin >> numeros1[j];
+            if (numeros1[j] == 1) {
+            suma++;
+            }
+        }
+         /*if(numeros1[j]!=1 && numeros1[j]!=0){
+            cout<<"Numero incorrecto, vuelve a intentarlo\n";
+            cout << "Ingrese 1 si hay mina y 0 si no hay mina: " << endl; 
+            cin >> numeros1[j];
+            if (numeros1[j] == 1) {
+            suma++;
+            }
+
+        }*/
     }
-    for (int i = 0; i < cant1; i++)
-    {
-        cout<<"Las minas son: "<<suma;
+    cout << "Las minas son: ";
+    for (int i = 0; i < cant1; i++) {
+        cout << numeros1[i] << " ";
     }
+    cout << endl;
+    cout << "El total de minas es: " << suma << endl;
+
+    return 0;
 }
+
